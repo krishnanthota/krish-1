a5be1f4f314e0410414766fbcf845456f22908d5
Krishnanjaneyulu <106869493+Krishnanjaneyulu@users.noreply.github.com>
2022-06-04 17:27:25 +0530

    one.jave file added

---
+++ one.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class one 
+{
+  public static void main(string[]args)
+  {
+    system.out.ptintIn("welcome to Git operation.....");
+      system.out.ptintIn("welcome to Git operation.....");
+      system.out.ptintIn("welcome to Git operation.....");
+  }
+}  
